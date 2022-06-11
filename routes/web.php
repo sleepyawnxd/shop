ae2a7a51e1899e754f8b49b9430dc38d47994cad
sleepy <sleepyawn@yandex.ru>
2022-06-11 21:23:07 +0500

    second commit

--- routes/web.php
+++ routes/web.php
@@ -50,3 +50,6 @@ Route::group(['namespace' => 'App\Http\Controllers\Admin','prefix' =>'admin'],fu
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+
